65a80c8007a1a003a51fff407e11a640710dda5f
Abdalrhman <se.abdalrhmanahmed@gmail.com>
2025-02-23 18:18:27 +0200

    up

---
+++ ios/flutter_export_environment.sh
@@ -0,0 +1,7 @@
+export FLUTTER_ROOT="$HOME/flutter"
+export FLUTTER_APPLICATION_PATH="$(pwd)"
+export FLUTTER_TARGET="lib/main.dart"
+export FLUTTER_BUILD_DIR="build"
+export SYMROOT="${SOURCE_ROOT}/../build/ios"
+export FLUTTER_FRAMEWORK_DIR="$FLUTTER_ROOT/bin/cache/artifacts/engine/ios"
+export OTHER_LDFLAGS="$(inherited) -framework Flutter"
